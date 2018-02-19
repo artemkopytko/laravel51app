106ffb5e64167781494ba229b96c959eb439c5e8
artemkopytko <kopytkoartem@gmail.com>
2018-02-19 14:09:48 +0200

    Unit tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,11 +1,13 @@
 <?php
 
+use App;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+	use DatabaseTransactions;
     /**
      * A basic functional test example.
      *
@@ -13,7 +15,24 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+	    $first = factory(App\Post::class)->create();
+		$second = factory(App\Post::class)->create([
+			'created_at' => \Carbon\Carbon::now()->subMonth()
+		]);
+
+    	$posts = App\Post::archives();
+
+    	$this->assertEquals([
+    		[
+			    "month" => $first->created_at->format('F').' ',
+                "year" => $first->created_at->format('Y'),
+			    "published" => 1
+		    ],
+		    [
+			    "month" => $second->created_at->format('F').'  ',
+			    "year" => $second->created_at->format('Y'),
+			    "published" => 1
+		    ]
+	    ], $posts);
     }
 }
